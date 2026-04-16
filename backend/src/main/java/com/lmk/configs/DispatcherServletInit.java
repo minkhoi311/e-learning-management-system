1a5f0dd904abfeb027bfaf9f01a9956ea6ccd9a0
Khoi <minkhoi.dev@gmail.com>
2026-04-16 23:49:37 +0700

    start project

---
+++ backend/src/main/java/com/lmk/configs/DispatcherServletInit.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.lmk.configs;
+
+import jakarta.servlet.MultipartConfigElement;
+import jakarta.servlet.ServletRegistration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ *
+ * @author Acer
+ */
+public class DispatcherServletInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{
+            ThymeleafConfigs.class,
+            HibernateConfigs.class,
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{
+            WebAppContextConfigs.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String [] {"/"};
+    }
+    
+    //ghi đè để custome ảnh
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setMultipartConfig(new MultipartConfigElement("/", 5000000, 15000000, 0));
+    }
+    
+}
---
+++ backend/src/main/java/com/lmk/configs/WebAppContextConfigs.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.lmk.configs;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import com.lmk.formatters.CategoryFormatter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author Acer
+ */
+@Configuration
+@ComponentScan(
+        basePackages = {
+            "com.lmk.controllers",
+            "com.lmk.repositories",
+            "com.lmk.services",}
+)
+@EnableWebMvc
+@EnableTransactionManagement
+public class WebAppContextConfigs implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatter(new CategoryFormatter());
+    }
+
+    @Bean
+    public Cloudinary cloudinary() {
+        Cloudinary cloudinary
+                = new Cloudinary(ObjectUtils.asMap(
+                        "cloud_name", "dhin65fs8",
+                        "api_key", "198184884121951",
+                        "api_secret", "NWBqTXUf6LwdDRKLEW9Y1wqxbYA",
+                        "secure", true));
+        return cloudinary;
+    }
+    
+    @Bean
+    public StandardServletMultipartResolver multipartResolver(){
+        return new StandardServletMultipartResolver();
+    }
+}
